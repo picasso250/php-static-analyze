349916a6f7e0df31f42f27a05ec32150f0d1d0f3
wangxiaochi <wangxiaochi@baidu.com>
2015-04-20 13:34:27 +0800

    this

--- dup-func.php
+++ dup-func.php
@@ -4,7 +4,9 @@ require (__DIR__).'/vendor/autoload.php';
 require (__DIR__).'/autoload.php';
 require (__DIR__).'/logic.php';
 
-$table = [];
+$table = [
+    'Yii' => ['app' => 0],
+];
 $ignore = ['vendor'];
 handle_dir($argv[1], 'read_func'); // build
 handle_dir($argv[1], 'consume_func'); // consume
@@ -20,7 +22,12 @@ foreach ($table as $c => $value) {
 
 function is_method_ignore($method)
 {
-    $ignore_method_pattern_list = ['/^action[A-Z]/', '/^__/'];
+    $ignore_method_pattern_list = [
+        '/^action[A-Z]/',
+        '/^__/',
+        '/^tableName$/',
+        '/^beforeAction$/'
+    ];
     foreach ($ignore_method_pattern_list as $pattern) {
         if (preg_match($pattern, $method)) {
             return true;
@@ -40,7 +47,7 @@ function handle_dir($dir, $callback)
             if (is_dir("$dir/$file")) {
                 handle_dir("$dir/$file", $callback);
             } elseif (preg_match('/\.php$/', $file)) {
-                echo "process $dir/$file\n";
+                error_log("process $dir/$file");
                 $callback("$dir/$file");
             }
         }
@@ -56,7 +63,7 @@ function read_func($file)
     $stmts = $parser->parse($code);
     foreach ($stmts as $stmt) {
         if ($stmt instanceof PhpParser\Node\Stmt\Class_) {
-            echo "class $stmt->name\n";
+            error_log("class $stmt->name");
             foreach ($stmt->stmts as $s) {
                 if ($s instanceof PhpParser\Node\Stmt\ClassMethod) {
                     // echo "find method $s->name\n";
@@ -81,6 +88,13 @@ function consume_func($file)
 function _consume_func($stmts)
 {
     foreach ($stmts as $stmt) {
+        if ($stmt instanceof PhpParser\Node\Stmt\Class_) {
+            $GLOBALS['current_class'] = $stmt->name;
+            if ($stmt->extends) {
+                assert(count($stmt->extends->parts) == 1);
+                $GLOBALS['current_class_parent'] = $stmt->extends->parts[0];
+            }
+        }
         if ($stmt->stmts || $stmt->stmts === array()) {
             _consume_func($stmt->stmts);
         } elseif ($stmt instanceof PhpParser\Node\Stmt\Unset_) {
@@ -160,10 +174,21 @@ function expr_consume($expr)
     } elseif (is_expr($expr, ['PreInc', 'PostInc', 'PreDec', 'PostDec'])) {
         expr_consume($expr->var);
     } elseif ($expr instanceof PhpParser\Node\Expr\StaticCall) {
+        assert(count($expr->class->parts) == 1);
         $class = $expr->class->parts[0];
+        if ($class == 'self') {
+            $class = $GLOBALS['current_class'];
+        } elseif ($class == 'parent') {
+            $class = $GLOBALS['current_class_parent'];
+        }
         class_method_incr($class, $expr->name);
     } elseif ($expr instanceof PhpParser\Node\Expr\MethodCall) {
-        all_method_incr($expr->name);
+        $var = $expr->var;
+        if ($var->name === 'this') { // inheretation
+            class_method_incr($GLOBALS['current_class'], $expr->name);
+        } else {
+            all_method_incr($expr->name);
+        }
     } elseif ($expr instanceof PhpParser\Node\Expr\FuncCall) {
         // do nothing
     } else {
